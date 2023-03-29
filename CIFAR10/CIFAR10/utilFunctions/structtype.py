c5dbb7cb3a3167a3f0ab525bdda5629f964cf6af
Lorenzo Corrado <lorenzo.corrado1997@gmail.com>
2023-03-29 19:20:48 +0200

    Add files via upload

---
+++ CIFAR10/CIFAR10/utilFunctions/structtype.py
@@ -0,0 +1,9 @@
+class structtype:
+    def __init__(self, **kwargs):
+        self.Set(**kwargs)
+
+    def Set(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def SetAttr(self, lab, val):
+        self.__dict__[lab] = val
